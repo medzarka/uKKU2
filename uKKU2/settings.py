0e340f7c59e8b67581f47f5782368999ed3ac766
mzarka <medzarka@gmail.com>
2022-06-18 10:55:50 +0300

    update mysql according to namecheap.

--- uKKU2/settings.py
+++ uKKU2/settings.py
@@ -105,11 +105,11 @@ DATABASES = {
         'PASSWORD': site_conf.site_BD_PASSWORD,
         'HOST': site_conf.site_BD_HOST,
         'PORT': site_conf.site_BD_PORT,
-        'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES', innodb_strict_mode=1",
-            'charset': 'utf8mb4',
-            "autocommit": True,
-        }
+        #'OPTIONS': {
+        #    'init_command': "SET sql_mode='STRICT_TRANS_TABLES', innodb_strict_mode=1",
+        #    'charset': 'utf8mb4',
+        #    "autocommit": True,
+        #}
 
     }
 }
