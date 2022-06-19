e7b1025d583ac93c52992cbd0fdd42513b7c9c7d
mzarka <medzarka@gmail.com>
2022-06-19 20:28:00 +0300

    configure possibility to load config file from string given in os environment 3 .

--- uKKU2/settings.py
+++ uKKU2/settings.py
@@ -16,12 +16,13 @@ from django.utils.module_loading import import_string
 
 
 try:
-    conf_filename = os.getenv('django_site_configurations')
-    site_conf = import_string(f'uKKU2.{conf_filename}')
-    print(f'Configuration Site in file {conf_filename}.')
-except:
-    import uKKU2.site_conf as site_conf
 
+    conf_filename = os.getenv('django_site_configurations')
+    print(f'Configuration Site in file {conf_filename}.')
+    site_conf = import_string(f'uKKU2.{conf_filename}')
+
+except ModuleNotFoundError:
+    import uKKU2.site_conf as site_conf
     print(f'Configuration Site in file "site_conf".')
 
 ##" https://github.com/medzarka/uKKU2.git
